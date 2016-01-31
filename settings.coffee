b17825c271f43119a0b3339c19b5c48c4ddfdac7
Ville Talonpoika <talonvi@gmail.com>
2016-01-31 19:01:39 +0200

    Added html

--- settings.coffee
+++ settings.coffee
@@ -1,1 +1,30 @@
-asd = 5;
+typeface = "Times New Roman"
+size = "100%"
+
+readData = ->
+  typeface = loadCookie("typeface")
+  size = loadCookie("size")
+  console.log(typeface + ", " + loadCookie(typeface))
+
+loadCookie = (cname) ->
+  name = cname + '='
+  ca = document.cookie.split(';')
+  i = 0
+  while i < ca.length
+    c = ca[i]
+    while c.charAt(0) == ' '
+      c = c.substring(1)
+    if c.indexOf(name) == 0
+      return c.substring(name.length, c.length)
+    i++
+  ''
+
+saveCookie = (cname, cvalue, exdays) ->
+  d = new Date
+  d.setTime d.getTime() + exdays * 24 * 60 * 60 * 1000
+  expires = 'expires=' + d.toUTCString()
+  document.cookie = cname + '=' + cvalue + '; ' + expires
+  console.log(document.cookie)
+
+saveCookie("typeface","Times New Roman",365)
+readData()
--- settings.js
+++ settings.js
@@ -1,3 +1,42 @@
-var asd;
+var loadCookie, readData, saveCookie, size, typeface;
 
-asd = 5;
+typeface = "Times New Roman";
+
+size = "100%";
+
+readData = function() {
+  typeface = loadCookie("typeface");
+  size = loadCookie("size");
+  return console.log(typeface + ", " + loadCookie(typeface));
+};
+
+loadCookie = function(cname) {
+  var c, ca, i, name;
+  name = cname + '=';
+  ca = document.cookie.split(';');
+  i = 0;
+  while (i < ca.length) {
+    c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+    i++;
+  }
+  return '';
+};
+
+saveCookie = function(cname, cvalue, exdays) {
+  var d, expires;
+  d = new Date;
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + '; ' + expires;
+  return console.log(document.cookie);
+};
+
+saveCookie("typeface", "Times New Roman", 365);
+
+readData();
